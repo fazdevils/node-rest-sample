0fd8a5b77fb1bfb8fe42f955295d9809bc807db0
Vincent Fazio <vincent.fazio@gmail.com>
2020-03-09 16:41:14 -0400

    slight refactoring in code used to generate json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
 /* IMPORT ROUTES */
-const swaggerUi = require('express-oas-generator')
+const swaggerUi = require('express-oas-generator') // DO NOT USE IN PRODUCTION!!
 const utilRoutes = require('./routes/util.js')
 const peopleRoutes = require('./routes/people.js')
 
@@ -13,13 +13,15 @@ const peopleRoutes = require('./routes/people.js')
 const xpserver = express()
 
 /* SERVER CONFIGURATION */
+swaggerUi.handleResponses(xpserver, {})
+
 xpserver.use(morgan('common')) // logging
 xpserver.use(express.static('./public'))
 xpserver.use(bodyParser.urlencoded({extended: false}))
-swaggerUi.init(xpserver, {})
 xpserver.use('/', utilRoutes)
 xpserver.use('/people', peopleRoutes)
 
 /* START THE SERVER */
+swaggerUi.handleRequests()
 xpserver.listen(8080, () => {
 })
